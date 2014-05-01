ec43a53b1b87b5921a59dd72cd9073e2ecfe970c
Nikita <nikzyul@ya.ru>
2014-05-01 23:12:01 +0400

    index page impl

--- src/data/default.js
+++ src/data/default.js
@@ -2,4 +2,10 @@ var id = 0;
 
 exports.genId = function() {
 	return 'gen-' + (++id);
-}
+};
+
+exports.trials = [
+    { title: "Java", desc: "Simple Java trial" },
+    { title: "Python 3", desc: "Simple Py3 trial" },
+    { title: "JavaScript", desc: "Simple JS trial" }
+];
